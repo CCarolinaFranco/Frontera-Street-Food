1f17e3480e3b71e4282625f3808dc7c00163d583
CCarolinaFranco <cirilina@hotmail.com>
2023-11-08 11:14:51 +0000

    section services htm

--- js/main.js
+++ js/main.js
@@ -2,4 +2,31 @@
 
 console.log("holis");
 
+// slider de productos
+
+const contenedor = document.querySelector('.js-slider');
+const btnIzquierda = document.querySelector('.js-btn-izquierda');
+const btnDerecha = document.querySelector('.js-btn-derecha');
+
+
+
+//evento para el boton derecho y izquierda slider
+
+
+function handleClickDerecha(){
+    contenedor.scrollLeft += contenedor.offsetWidth;
+
+}
+
+btnDerecha.addEventListener("click", handleClickDerecha);
+
+
+function handleClickIzquierda(){
+    contenedor.scrollLeft -= contenedor.offsetWidth;
+    
+}
+
+btnIzquierda.addEventListener("click", handleClickIzquierda);
+
+
 
