bb7622aae4ab1b5cd32db88dfa8a4d0a9cfb4319
CCarolinaFranco <cirilina@hotmail.com>
2023-11-21 11:00:33 +0000

    fields validation

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ console.log("holis");
 const contenedor = document.querySelector('.js-slider');
 const btnIzquierda = document.querySelector('.js-btn-izquierda');
 const btnDerecha = document.querySelector('.js-btn-derecha');
+const formulario = document.querySelector('.js-formulario');
+const inputName = document.querySelector('.js-inputName');
+const inputEmail = document.querySelector('.js-inputEmail');
+const inputComments = document.querySelector('.js-inputComments');
+const alertSuccess = document.querySelector('.js-alertSuccess');
+const alertError = document.querySelector('.js-alertError');
+
+
 
 
 
@@ -29,4 +37,43 @@ function handleClickIzquierda(){
 btnIzquierda.addEventListener("click", handleClickIzquierda);
 
 
+//validacion de formulario
 
+function validar(e){
+    let inputNameValue =inputName.value;
+    let inputEmailValue = inputEmail.value;
+    let inputCommentsValue =inputComments.value;
+
+    if(inputNameValue === "" || inputEmailValue === "" || inputCommentsValue === "" ){
+        e.preventDefault();
+        alertError.classList.remove("hide");
+        alertError.classList.add("show");
+
+        setTimeout(function(){
+            alertError.classList.remove("show");
+            alertError.classList.add("hide");  
+        }, 2000);
+
+
+    }
+    else{
+        e.preventDefault();
+        alertSuccess.classList.remove("hide");
+        alertSuccess.classList.add("show");
+
+        setTimeout(function(){
+            alertSuccess.classList.remove("show");
+            alertSuccess.classList.add("hide");  
+        }, 2000);
+
+            inputName.value = ""; // Clear the input values
+            inputEmail.value = "";
+            inputComments.value = "";
+
+    }
+
+}
+
+
+
+formulario.addEventListener("submit", validar);
